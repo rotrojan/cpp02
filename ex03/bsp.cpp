fc9215f825499e0f593f97dcd7a6adbd5e0ddf17
Romain TROJANI-BIGOT <rotrojan@e2r3p8.clusters.42paris.fr>
2021-11-30 23:46:00 +0100

    Post-evaluation fix: fixed the case where a division by zero occurs and where the three points of the triangle are collinear.

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,24 +6,39 @@
 /*   By: rotrojan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 23:30:55 by rotrojan          #+#    #+#             */
-/*   Updated: 2021/11/30 18:18:46 by rotrojan         ###   ########.fr       */
+/*   Updated: 2021/11/30 23:42:28 by rotrojan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
+bool	point_are_collinear(Point const a, Point const b, Point const c)
+{
+	return (a.getX() * (b.getY() - c.getY())
+		+ b.getX() * (c.getY() - a.getY())
+		+ c.getX() * (a.getY() - b.getY()) == 0);
+}
+
 bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
+	if (point_are_collinear(a, b, c) == true)
+		return (false);
+	Fixed	quotient_w1(
+			(b.getY() - a.getY()) * (c.getX() - a.getX())
+				- (b.getX() - a.getX()) * (c.getY() - a.getY())
+	);
+	Fixed	quotient_w2(c.getY() - a.getY());
+	if (quotient_w1 == 0 || quotient_w2 == 0)
+		return (bsp(c, a, b, point));
 	Fixed	w1(
 		(a.getX() * (c.getY() - a.getY())
 			+ (point.getY() - a.getY()) * (c.getX() - a.getX())
 			- point.getX() * (c.getY() - a.getY()))
-		/ ((b.getY() - a.getY()) * (c.getX() - a.getX())
-				- (b.getX() - a.getX()) * (c.getY() - a.getY()))
+		/ quotient_w1
 	);
 	Fixed	w2(
 		(point.getY() - a.getY() - w1 * (b.getY() - a.getY()))
-		/ (c.getY() - a.getY())
+		/ quotient_w2
 	);
 	return (w1 > 0 && w2 > 0 && w1 + w2 < 1);
 }
